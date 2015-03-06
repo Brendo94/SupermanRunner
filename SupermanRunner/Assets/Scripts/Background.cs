f7dd398eb0f448e7f3f251e3e86d11ac30c12091
marcosico <joao.marcos.ico@hotmail.com>
2015-03-06 20:28:07 -0300

    Adição do personagem e background

---
+++ SupermanRunner/Assets/Scripts/Background.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Background : MonoBehaviour {
+
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		transform.position = new Vector3 (transform.position.x, transform.position.y - 0.01f);
+
+	}
+
+	void OnCollisionEnter2D(Collision2D coll){
+
+				transform.position = new Vector3 (0, 0.89f, 0);
+
+	}
+}
---
+++ SupermanRunner/Assets/Scripts/Player.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+	Vector3 direita = new Vector3(1.22f, -3.6f,-1);
+	Vector3 esquerda = new Vector3(-1.22f, -3.6f,-1);
+	// Use this for initialization
+	void Start () {
+		transform.position = esquerda;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey ("right")) {
+			transform.position = direita;
+		}else if(Input.GetKey("left")){
+			transform.position = esquerda;
+		}
+	}
+}
