1b82bad8b6d80aa6cfbb0706e7d0e97598fc3726
Neils01 <iurryy@yahoo.com.br>
2015-03-13 20:29:30 -0300

    destruição da criptonita
    Adicionando destruição da criptonita

---
+++ SupermanRunner/Assets/Scripts/Criptonita.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Criptonita : MonoBehaviour {
+	public GameObject mini_Crip;
+	ForceMode2D forca;
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter2D(Collision2D coll) {
+		if (coll.gameObject.tag == "Player")
+			Destroy (this.gameObject);
+
+	}
+
+	void OnTriggerEnter2D(Collider2D coll) {
+		if (coll.gameObject.tag == "Player") {
+			for (int cont=0;cont<5;cont++) {
+				GameObject nome = GameObject.Instantiate(mini_Crip) as GameObject;
+				nome.transform.position = this.gameObject.transform.position;
+				if(cont%2==0) {
+					nome.rigidbody2D.AddForce(new Vector2(Random.value*1000,Random.value*1000));
+				} else {
+					nome.rigidbody2D.AddForce(new Vector2(-Random.value*1000,Random.value*1000));
+				}
+
+
+			}
+			Destroy (this.gameObject);
+			
+			//Application.loadLevel();
+		}
+	}
+}
--- SupermanRunner/Assets/Scripts/Player.cs
+++ SupermanRunner/Assets/Scripts/Player.cs
@@ -1,12 +1,14 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 public class Player : MonoBehaviour {
 	Vector3 direita = new Vector3(1.22f, -3.6f,-1);
 	Vector3 esquerda = new Vector3(-1.22f, -3.6f,-1);
+	int vida;
 	// Use this for initialization
 	void Start () {
 		transform.position = esquerda;
+		vida = 3;
 	}
 	
 	// Update is called once per frame
@@ -16,11 +18,17 @@ public class Player : MonoBehaviour {
 		}else if(Input.GetKey("left")){
 			transform.position = esquerda;
 		}
+		if(vida==0) {
+			Destroy (this.gameObject);
+		}
 	}
 
 	void OnTriggerEnter2D(Collider2D coll) {
-		if (coll.gameObject.tag == "enemy")
-			Destroy(this.gameObject);
+		if (coll.gameObject.tag == "enemy") {
+			vida = vida - 1;
+
+			//Application.loadLevel();
+		}
 	}
 
 }
